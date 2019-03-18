0f19e991c841070e5dc0faf421bf4a72304fbd96
lzkSteven <linzeke@vt.edu>
2019-03-19 05:39:18 +0800

    Add files via upload

---
+++ Table&balls/js/bingo.js
@@ -0,0 +1,23 @@
+function numberGenerator() {
+  var n = new Array(25);
+  var nums = new Array(25);
+  for (let i = 0; i < nums.length; i++) {
+    var randomNumber = Math.floor(Math.random() * 70);
+    for (let j = 0; j < n.length; j++) {
+      if (n[j] == randomNumber)
+        randomNumber = Math.floor(Math.random() * 70);
+    }
+    n[i] = randomNumber;
+  }
+      // Output to the table
+      for (let k = 0; k < n.length; k++) {
+        // document.getElementById("square" + k).style.fontsize = "100px";
+        document.getElementById("square" + k).innerText = (n[k]);
+        document.getElementById("square" + k).style.position = "relative";
+        document.getElementById("square" + k).style.top = "30%"
+        document.getElementById("square" + k).align = "center";
+      }
+      document.getElementById("square12").innerHTML = "";
+      document.getElementById("square12").style = "none";
+      // document.getElementById("ball1").append(numOnBall);
+    }
