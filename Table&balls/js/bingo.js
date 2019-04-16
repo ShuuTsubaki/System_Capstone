0e09808398dd909cee0be9980fecf32747f5fdad
lzkSteven <linzeke@vt.edu>
2019-04-16 13:42:05 +0800

    Add files via upload
    UI complete

--- Table&balls/js/bingo.js
+++ Table&balls/js/bingo.js
@@ -9,15 +9,30 @@ function numberGenerator() {
     }
     n[i] = randomNumber;
   }
-      // Output to the table
-      for (let k = 0; k < n.length; k++) {
-        // document.getElementById("square" + k).style.fontsize = "100px";
-        document.getElementById("square" + k).innerText = (n[k]);
-        document.getElementById("square" + k).style.position = "relative";
-        document.getElementById("square" + k).style.top = "30%"
-        document.getElementById("square" + k).align = "center";
-      }
-      document.getElementById("square12").innerHTML = "";
-      document.getElementById("square12").style = "none";
-      // document.getElementById("ball1").append(numOnBall);
-    }
+  // Output to the table
+  for (let k = 0; k < n.length; k++) {
+    // document.getElementById("square" + k).style.fontsize = "100px";
+    document.getElementById("square" + k).innerText = (n[k]);
+    document.getElementById("square" + k).style.position = "relative";
+    document.getElementById("square" + k).style.top = "30%"
+    document.getElementById("square" + k).align = "center";
+  }
+  document.getElementById("square12").innerHTML = "";
+  document.getElementById("square12").style = "none";
+  
+  // document.getElementById("num1").innerText = (33);
+  // document.getElementById("num1").style.position = "relative";
+  // document.getElementById("num1").style.top = "30%"
+  // document.getElementById("num1").align = "center";
+}
+function putNumber(arr) {
+  for (let k = 0; k < arr.length; k++) {
+    // document.getElementById("square" + k).style.fontsize = "100px";
+    document.getElementById("square" + k).innerText = (arr[k]);
+    document.getElementById("square" + k).style.position = "relative";
+    document.getElementById("square" + k).style.top = "30%"
+    document.getElementById("square" + k).align = "center";
+  }
+  document.getElementById("square12").innerHTML = "";
+  document.getElementById("square12").style = "none";
+}
