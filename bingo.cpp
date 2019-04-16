3a63c924a6a3d0b673769abfe1ec20f496e7d43e
nap64 <nap64@vt.edu>
2019-04-16 16:25:54 -0400

    updated contract.

--- bingo.cpp
+++ bingo.cpp
@@ -1,5 +1,6 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/crypto.h>
+#include <eosiolib/transaction.h>
 
 using namespace std;
 using namespace eosio;
@@ -15,6 +16,7 @@ class [[eosio::contract]] bingo : public eosio::contract
             name        username;
             uint16_t    win_count;
             uint16_t    lost_count;
+            int8_t      prev_game;
             
             uint64_t    game_id;
             bool        in_game;
@@ -24,7 +26,7 @@ class [[eosio::contract]] bingo : public eosio::contract
             std::vector<int8_t> board;//[GAME_SIZE * GAME_SIZE];
             std::vector<int8_t> balls;//[GAME_SIZE * 2];
             int8_t iteration;
-            std::string block_seed;
+            capi_checksum256 block_seed;
             
             uint64_t primary_key() const { return username.value; }
             uint64_t get_secondary_1() const { return game_id; }
@@ -47,17 +49,6 @@ class [[eosio::contract]] bingo : public eosio::contract
         
         players_table _players;
         record_table _record;
-        
-        capi_checksum256 gen_rand(std::string str1, std::string str2)
-        {
-            char* datanew = new char[str1.length() + str2.length() + 2];
-            strcpy(datanew, str1.c_str());
-            strcpy(datanew + str1.length() + 1, str2.c_str());
-            capi_checksum256 h;
-            sha256(datanew, str1.length() + str2.length() + 2, &h);
-            delete [] datanew;
-            return h;
-        }
 
     public:
         using contract::contract;
@@ -93,9 +84,9 @@ class [[eosio::contract]] bingo : public eosio::contract
                         new_user.username = username;
                         new_user.win_count = 0;
                         new_user.lost_count = 0;
+                        new_user.prev_game = -1;
                         new_user.in_game = false;
                         new_user.seed = seed;
-                        new_user.block_seed = "";
                         new_user.game_id = itr->game_id;
                         new_user.board.reserve(GAME_SIZE * GAME_SIZE);
                         new_user.balls.reserve(GAME_SIZE * 2);
@@ -115,7 +106,6 @@ class [[eosio::contract]] bingo : public eosio::contract
                     {
                         player.seed = seed;
                         player.in_game = false;
-                        player.block_seed = "";
                         player.game_id = itr->game_id;
                     }
                 );
@@ -150,7 +140,10 @@ class [[eosio::contract]] bingo : public eosio::contract
                 iterator, username, [&](auto& player)
                 {
                     if (player.in_game)
+                    {
                         player.lost_count = player.lost_count + 1;
+                        player.prev_game = 0;
+                    }
                     else if (player.game_id == itr->game_id && itr->cur_group_count != 0)
                         _record.modify
                         (
@@ -168,7 +161,7 @@ class [[eosio::contract]] bingo : public eosio::contract
         //call this REPEATEDLY until in_game is true
         //table will be generated
         [[eosio::action]]
-        void entergame(name username, std::string b_seed)
+        void entergame(name username)
         {
             require_auth(username);
             auto ritr = _record.begin();
@@ -181,26 +174,15 @@ class [[eosio::contract]] bingo : public eosio::contract
             (
                 iterator, username, [&](auto& player)
                 {
-                    bool seed_flag = true;
-                    player.block_seed = "";
-                    auto ptable = _players.get_index<"bygid"_n>();
-                    auto itr = ptable.lower_bound(player.game_id);
-                    while (itr != ptable.end())
-                    {
-                        if (itr->game_id != player.game_id)
-                            break;
-                        if (itr->in_game && itr->block_seed.compare("") != 0)
-                        {
-                            seed_flag = false;
-                            player.block_seed = itr->block_seed;
-                        }
-                        itr++;
-                    }
-                    if (seed_flag)
-                        player.block_seed = b_seed;
                     player.in_game = true;
                     player.iteration = 0;
-                    capi_checksum256 hash = gen_rand(player.seed, player.block_seed);
+                    auto size = transaction_size();
+                    char* buf = new char[size + strlen(player.seed.c_str())];
+                    uint32_t read = read_transaction(buf, size);
+                    strcpy(buf + read, player.seed.c_str());
+                    capi_checksum256 hash;
+                    sha256(buf, read + strlen(player.seed.c_str()), &hash);
+                    delete [] buf;
                     int8_t* t = new int8_t[GAME_SIZE * GAME_SIZE];
                     int8_t i, k = 0;
                     for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
@@ -222,29 +204,9 @@ class [[eosio::contract]] bingo : public eosio::contract
                         player.board[i] = t[k];
                         t[k] = -1;
                     }
-                    for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
-                        t[i] = i;
-                    k = 0;
-                    for (i = 0; i < GAME_SIZE; i++)
-                    {
-                        int8_t j = ((uint8_t)hash.hash[i + GAME_SIZE * GAME_SIZE]) % (GAME_SIZE * GAME_SIZE - i);
-                        while (1)
-                        {
-                            if (t[k] >= 0)
-                            {
-                                j--;
-                                if (j == -1)
-                                    break;
-                            }
-                            k++;
-                            k = k % (GAME_SIZE * GAME_SIZE);
-                        }
-                        player.balls[i] = t[k];
-                        t[k] = -1;
-                    }
                     delete [] t;
-                    for (i = 0; i < GAME_SIZE; i++)
-                        player.balls[GAME_SIZE + i] = -1;
+                    for (i = 0; i < GAME_SIZE * 2; i++)
+                        player.balls[i] = -1;
                 }
             );
         }
@@ -253,7 +215,7 @@ class [[eosio::contract]] bingo : public eosio::contract
         //For total of GAME_SIZE + 1 times
         //the last time is to end game
         [[eosio::action]]
-        void genball(name username, std::string b_seed)
+        void genball(name username)
         {
             require_auth(username);
             auto ritr = _record.begin();
@@ -261,7 +223,7 @@ class [[eosio::contract]] bingo : public eosio::contract
             auto iterator = _players.find(username.value);
             eosio_assert(iterator != _players.end(), "Player does not exist");
             eosio_assert(iterator->in_game, "Player not in game!");
-            if (iterator->iteration >= GAME_SIZE)
+            if (iterator->iteration > GAME_SIZE)
             {
                 logout(username);
                 return;
@@ -270,7 +232,6 @@ class [[eosio::contract]] bingo : public eosio::contract
             (
                 iterator, username, [&](auto& player)
                 {
-                    bool seed_flag = true;
                     bool end_flag = true;
                     auto ptable = _players.get_index<"bygid"_n>();
                     auto itr = ptable.lower_bound(player.game_id);
@@ -283,51 +244,109 @@ class [[eosio::contract]] bingo : public eosio::contract
                             end_flag = false;
                             break;
                         }
-                        if (itr->in_game && itr->block_seed.compare("") != 0 && itr->iteration == player.iteration)
-                        {
-                            seed_flag = false;
-                            player.block_seed = itr->block_seed;
-                        }
                         itr++;
                     }
                     if (end_flag)
                     {
-                        if (seed_flag)
-                            player.block_seed = b_seed;
-                        player.iteration = player.iteration + 1;
-                        capi_checksum256 hash = gen_rand(player.seed, player.block_seed);
-                        uint64_t r = hash.hash[1];
-                        r = r << 32;
-                        r |= hash.hash[0];
-                        int8_t* t = new int8_t[GAME_SIZE * GAME_SIZE];
-                        int8_t i, k = 0;
-                        for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
-                            t[i] = i;
-                        for (i = 0; i < GAME_SIZE; i++)
-                            if (player.balls[GAME_SIZE + i] >= 0)
-                                t[player.balls[GAME_SIZE + i]] = -1;
-                            else
-                                break;
-                        int8_t j = r % (GAME_SIZE * GAME_SIZE - i);
-                        while (1)
+                        if (player.iteration == 0) //generate seed and first 5 balls
                         {
-                            if (t[k] >= 0)
+                            auto ptable = _players.get_index<"bygid"_n>();
+                            auto itr = ptable.lower_bound(player.game_id);
+                            std::string all_seed = "";
+                            bool s_flag = true;
+                            while (itr != ptable.end())
                             {
-                                j--;
-                                if (j == -1)
+                                if (itr->game_id != player.game_id)
                                     break;
+                                if (itr->iteration > player.iteration)
+                                {
+                                    s_flag = false;
+                                    int i;
+                                    for (i = 0; itr->balls[i] >= 0 && i < GAME_SIZE * 2; i++)
+                                        player.balls[i] = itr->balls[i];
+                                    player.iteration = itr->iteration;
+                                    player.block_seed = itr->block_seed;
+                                    break;
+                                }
+                                if (itr->in_game)
+                                    all_seed = all_seed + itr->seed;
+                                itr++;
                             }
-                            k++;
-                            k = k % (GAME_SIZE * GAME_SIZE);
+                            if (s_flag) //first 5 generation
+                            {
+                                player.iteration = player.iteration + 1;
+                                capi_checksum256 h;
+                                sha256(all_seed.c_str(), strlen(all_seed.c_str()), &h);
+                                int8_t* t = new int8_t[GAME_SIZE * GAME_SIZE];
+                                int i;
+                                for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
+                                    t[i] = i;
+                                int k = 0;
+                                for (i = 0; i < GAME_SIZE; i++)
+                                {
+                                    uint32_t j = h.hash[i * 4] << 24 | h.hash[i * 4 + 1] << 16 | h.hash[i * 4 + 2] << 8 | h.hash[i * 4 + 3];
+                                    j = j % (GAME_SIZE * GAME_SIZE - i);
+                                    while (1)
+                                    {
+                                        if (t[k] >= 0)
+                                        {
+                                            j--;
+                                            if (j == -1)
+                                                break;
+                                        }
+                                        k++;
+                                        k = k % (GAME_SIZE * GAME_SIZE);
+                                    }
+                                    player.balls[i] = t[k];
+                                    t[k] = -1;
+                                }
+                                delete [] t;
+
+                                auto size = transaction_size();
+                                char* buf = new char[size + strlen(all_seed.c_str())];
+                                uint32_t read = read_transaction(buf, size);
+                                strcpy(buf + read, player.seed.c_str());
+                                capi_checksum256 hash;
+                                sha256(buf, read + strlen(player.seed.c_str()), &hash);
+                                player.block_seed = hash;
+                            }
                         }
-                        player.balls[GAME_SIZE + i] = t[k];
-                        delete [] t;
-                        eosio_assert(i + 1 == player.iteration, "We have sanity problem?");
+                        else
+                        {
+                            player.iteration = player.iteration + 1;
+                            int8_t* t = new int8_t[GAME_SIZE * GAME_SIZE];
+                            int8_t i, k = 0;
+                            for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
+                                t[i] = i;
+                            for (i = 0; i < GAME_SIZE; i++)
+                                if (player.balls[GAME_SIZE + i] >= 0)
+                                    t[player.balls[GAME_SIZE + i]] = -1;
+                                else
+                                    break;
+                            i = player.iteration - 1;
+                            uint32_t j = player.block_seed.hash[i * 4] << 24 | player.block_seed.hash[i * 4 + 1] << 16 | player.block_seed.hash[i * 4 + 2] << 8 | player.block_seed.hash[i * 4 + 3];
+                            j = j % (GAME_SIZE * GAME_SIZE - i - 1);
+                            while (1)
+                            {
+                                if (t[k] >= 0)
+                                {
+                                    j--;
+                                    if (j == -1)
+                                        break;
+                                }
+                                k++;
+                                k = k % (GAME_SIZE * GAME_SIZE);
+                            }
+                            player.balls[GAME_SIZE + i - 1] = t[k];
+                            delete [] t;
+                            eosio_assert(i + 1 == player.iteration, "We have sanity problem?");
+                        }
                     }
                     else
                     {
                         player.iteration = -2;
                         player.lost_count = player.lost_count + 1;
+                        player.prev_game = 0;
                         player.in_game = false;
                     }
                 }
@@ -342,11 +361,6 @@ class [[eosio::contract]] bingo : public eosio::contract
             eosio_assert(ritr != _record.end(), "Player does not exist");
             auto iterator = _players.find(username.value);
             eosio_assert(iterator->in_game, "Player not in game!");
-            if (iterator->iteration >= GAME_SIZE) //timeout
-            {
-                logout(username);
-                return;
-            }
             _players.modify
             (
                 iterator, username, [&](auto& player)
@@ -368,33 +382,40 @@ class [[eosio::contract]] bingo : public eosio::contract
                     if (end_flag)
                     {
                         int8_t* t = new int8_t[GAME_SIZE * GAME_SIZE];
-                        int8_t i, j, r, c, w = 0;
+                        int8_t i, j, r, c, d, e, w = 0;
                         for (i = 0; i < GAME_SIZE * GAME_SIZE; i++)
                             t[i] = 0;
+                        t[12] = 1; //center bonus
                         for (i = 0; i < GAME_SIZE * 2; i++)
                             for (j = 0; j < GAME_SIZE * GAME_SIZE; j++)
                                 if (player.balls[i] == player.board[j])
                                     t[j] = 1;
+                        d = e = 1;
                         for (i = 0; i < GAME_SIZE; i++)
                         {
                             r = c = 1;
+                            d &= player.board[i * GAME_SIZE + i];
+                            e &= player.board[(i + 1) * GAME_SIZE - i - 1];
                             for (j = 0; j < GAME_SIZE; j++)
                             {
                                 r &= player.board[i * GAME_SIZE + j];
                                 c &= player.board[j * GAME_SIZE + i];
                             }
-                            w |= r | c;
+                            w |= r | c; // row & column
                         }
+                        w |= d | e; //diagnal
                         if (w == 1)
                         {
                             player.iteration = -1;
                             player.win_count = player.win_count + 1;
+                            player.prev_game = 1;
                             player.in_game = false;
                         }
                         else
                         {
                             player.iteration = -2;
                             player.lost_count = player.lost_count + 1;
+                            player.prev_game = 0;
                             player.in_game = false;
                         }
                     }
@@ -402,6 +423,7 @@ class [[eosio::contract]] bingo : public eosio::contract
                     {
                         player.iteration = -2;
                         player.lost_count = player.lost_count + 1;
+                        player.prev_game = 0;
                         player.in_game = false;
                     }
                 }
